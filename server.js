808c4093712929dea375e9e85a9f4df048d3b35c
Manuprasad-Cybera <Manu.whattodo@gmail.com>
2025-01-27 01:11:58 +0530

    test

--- server.js
+++ server.js
@@ -119,8 +119,12 @@ app.get('/dashboard', (req, res) => {
   }
 });
 
+app.get('/users', async (req, res) => {
+  res.send("sucess");
+});
+
 // Start server
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
